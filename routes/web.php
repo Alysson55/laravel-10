f4d1e54821dbfa425ccbedbcd98701c47101ce24
Alysson Santos <salysson280@gmail.com>
2023-06-28 16:46:46 -0300

    update commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/forum', [ForumController::class, 'index'])->name('index');
 
-Route::get('/contato', function () {
+Route::get('/contact', function () {
     return view('site/contact');
 });
 
